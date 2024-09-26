c063e71883d35651d29f9c8c0aa0e92ebfdef093
Recluse799 <ImmanuelBraxton@Gmail.com>
2024-09-26 03:45:24 -0400

    Working on code, and made vague pesudo code.

--- Second Project.cpp
+++ Second Project.cpp
@@ -17,9 +17,38 @@
 
 #include <cmath>
 
+using namespace std;
 
 int main()
 {
+    double FirstP; // First Product
+    double SecondP;
+    double Answer;
+    char Operand;
 
+
+    cout << "This program will calculate two products and a operand of your choosing.\n" << endl;
+    cout << "Please enter first product.\n" << endl;
+    cin >> FirstP;
+    cout << "Please enter operand.\n" << endl;
+    if (Operand != ("+")) || ("-") || ("*") || ("/"))
+    cin >> Operand;
+    
+    cout << "Please enter second product.\n" << endl;
+    cin >> SecondP;
+    
+    Answer = FirstP, Operand, SecondP;
+
+    cout << "Your answer is " << Answer << "." << endl;
 }
 
+/*
+* talk about program
+* ask for first number
+* ask for operand
+* figure out how to make error message for wrong operand
+* ask for last number
+* 
+* calculate don't for get about the remander in division
+* show final
+*/
