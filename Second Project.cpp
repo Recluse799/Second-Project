89bf61141b92725b93c1b0a62fe4a89b3d924657
Recluse799 <ImmanuelBraxton@Gmail.com>
2024-09-25 20:03:13 -0400

    saving

--- Second Project.cpp
+++ Second Project.cpp
@@ -1,46 +1,14 @@
 // Second Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* 
+*/
+
+
 
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-/*
-      Immanuel Braxton
-      9/25/2024
-      Project 1. Calculator
-      Description: Write a program that mimics a calculator. The program should take as input two
-      integers and the operation to be performed. It should then output the numbers, the
-      operator, and the result. For division, if the denominator is zero, output an
-      appropriate message. Limit the supported operations to + - / * and write an error
-      message if the operator is not one of the supported operations. Here is some
-      example output: 3 + 4 = 7, 13 * 5 = 65
-
- */
-
-#include <iostream>
-
-#include <iomanip>
-
-#include <cmath>
-
-
-int main()
-{
-
 }
 
