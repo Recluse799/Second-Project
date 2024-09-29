203b8f19844c374d109b7e67510a6229476ed7b4
Recluse799 <ImmanuelBraxton@Gmail.com>
2024-09-28 21:26:06 -0400

    Fixed the while loop problem, combined two input statements into one, fixed a looping error that caused the program to stop

--- Second Project.cpp
+++ Second Project.cpp
@@ -21,7 +21,68 @@ using namespace std;
 
 int main()
 {
-   
+	char op, choice, N, Y;
+	double num1, num2;
+	choice = 'Y';  // Initialize the choice variable
+
+
+	while (choice == 'Y') // loop while Y is equal to continue
+	{
+
+
+		cout << "Enter first integer, an operator (+, -, * or /), then last integer, include space between each item:"; // ask for operator, loop while i is equal to 1
+		cin >> num1 >> op >> num2;
+
+		//cout << "Enter two integers, include a space between integers: "; // ask for the two numbers, in while loop
+		//cin >> num1 >> num2;
+
+		if (op == '+')
+		{
+			cout << "total: " << num1 << "+" << num2 << "=" << (num1 + num2) << endl;
+		}
+		else if (op == '-')
+		{
+			cout << "total: " << num1 << "-" << num2 << "=" << (num1 - num2) << endl;
+		}
+		else if (op == '*')
+		{
+			cout << "total: " << num1 << "*" << num2 << "=" << (num1 * num2) << endl;
+		}
+		else if (op == '/')
+		{
+			if (num2 != 0)
+			{
+				cout << "total: " << num1 << "/" << num2 << "=" << (num1 / num2) << endl;
+			}
+			else
+			{
+				cout << "total: " << num1 << "/" << num2 << "=" << "Undefined" << endl;
+			}
+		}
+
+		else
+		{
+			cout << "Invalid operator " << endl;
+		}
+
+
+		cout << "Would you like to continue operating? Enter 'Y' or 'N' (case sensitive): " << endl;
+		cin >> choice;
+
+		while (choice != 'Y' && choice != 'N')
+		{
+			cout << "Invalid input! Please enter Y to continue or N to stop: " << endl;
+			cin >> choice;
+		}
+		if (choice == 'N')
+		{
+			break; //exit the loop if th user enters 'N'
+		}
+	}
+
+
+
+	return 0;
 
 }
 
